eba4b806310069e464fae09896355a34cb23bd6e
Stefan Bauer <stefan-a-bauer@gmx.net>
2015-04-06 20:27:20 +0200

    MainWindow: Added menu stub

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,21 @@
 #include "mainwindow.h"
 
+#include <QMenuBar>
+
 MainWindow::MainWindow()
 {
     _marble = new Marble::MarbleWidget();
     _marble->setProjection(Marble::Mercator);
     _marble->setMapThemeId("earth/openstreetmap/openstreetmap.dgml");
     setCentralWidget(_marble);
+
+    _importAction = new QAction(tr("&Import..."), this);
+    connect(_importAction, SIGNAL(triggered()), this, SLOT(import()));
+    _fileMenu = menuBar()->addMenu(tr("&File"));
+    _fileMenu->addAction(_importAction);
+}
+
+void MainWindow::import()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QAction>
 #include <marble/MarbleWidget.h>
 
 class MainWindow : public QMainWindow
@@ -11,8 +12,13 @@ class MainWindow : public QMainWindow
 public:
     MainWindow();
 
+private slots:
+    void import();
+
 private:
     Marble::MarbleWidget *_marble;
+    QMenu *_fileMenu;
+    QAction *_importAction;
 };
 
 #endif // MAINWINDOW_H
